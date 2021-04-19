9c8b8e10739aa1e6a4a7898702a1c2d0b65e2d67
Melvin Chidiebere <chidiebere.johnbosco@gmail.com>
2021-04-19 19:19:37 +0100

    further navbar updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c78e74c5757ea62044d8";
+/******/ 	var hotCurrentHash = "af02ef46d7f59f5fd3c9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
